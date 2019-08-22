671b356091b3af39043c65810f5b64d7c03cc012
dodzs <dodzie@gmail.com>
2019-08-22 14:57:21 +0800

    workshop03 - setup

--- workshop03/main.js
+++ workshop03/main.js
@@ -42,6 +42,94 @@ app.use(express.urlencoded({ extended: true }));
 // Start of workshop
 // TODO 2/2 Copy your routes from workshop02 here
 
+// TODO HEAD /api/state/:state
+// IMPORTANT: HEAD must be place before GET for the
+// same resource. Otherwise the GET handler will be invoked
+app.head('/api/state/:state',
+	(req,resp) => {
+		//should check if :state exists
+		resp.status(200);
+		resp.type('application/json');
+		resp.header('Accept-Ranges','cities')
+		resp.end()
+	}
+)
+
+// TODO GET /api/state/:state
+app.get('/api/state/:state',
+	range({accept : 'cities', limit:20}),
+	(req,resp) => {
+		const state=req.params.state;
+		const firstVar=req.range.first;
+		const lastVar=req.range.last;
+		Promise.all([
+			db.findCitiesByState(state,{offset:firstVar,limit:(lastVar-firstVar+1)}),
+			db.countCitiesInState(state)
+		]).then(results => {
+				resp.status(206);
+				resp.type('application/json');
+				resp.range({
+					first: firstVar,
+					second: lastVar,
+					length: results[1]
+				})
+				resp.json(
+					results[0].map(v=>`/api/city/${v}`)
+					);
+			})
+			.catch(error => {
+				resp.status(400);
+				resp.type('text/plain');
+				resp.send(error)
+			})
+	}
+)
+
+// TODO GET /api/city/:cityId
+app.get('/api/city/:varCityID',
+	(req,resp) => {
+		const city = req.params.varCityID;
+		db.findCityById(city)
+			.then(result => {
+				resp.type('application/json');
+				if(result.length>0){
+					resp.status(200);
+					resp.json(result[0]);
+				}
+				else{
+					resp.status(404);
+					resp.json(`message : cityID ${city} not found`);
+				}				
+			})
+			.catch(error => {
+				resp.status(400);
+				resp.type('text/plain');
+				resp.send({error})
+			})
+	}
+)
+
+// TODO POST /api/city
+// application/x-www-form-urlencoded - body
+app.post('/api/city',
+	(req,resp) => {
+		const data = req.body;
+		db.insertCity(data)
+			.then(result=>{
+				console.info('>> data:',data);
+				resp.status(201);
+				resp.type('application/json');
+				resp.json({ message:'added'}); 
+			})
+			.catch(error=>{
+				resp.status(400);
+				resp.type('text/plain');
+				resp.send({error})
+			})
+
+	}
+)
+
 
 // End of workshop
 
